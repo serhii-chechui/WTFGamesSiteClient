4046af6b55ad72d7378c9df4f54912efb29ddc73
Serhii Chechui <serhii.chechui@gmail.com>
2026-06-17 13:08:25 +0200

    feat(mainpage): fixes for main page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MainImage from "./MainImage";
+import "../styles/MainImage.css";
 
 const MainPage = () => {
     return (
         <div id="main-page">
-            <MainImage />
+            <div className="main-image__text">
+                <h1>watch the frame</h1>
+                <h2>Reality is changing</h2>
+            </div>
         </div>
     );
 };
